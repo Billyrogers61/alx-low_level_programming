3d97f7d259f5d14d9d342e12ac7de84f5fb429c3
Billyrogers61 <rogerskim61@gmail.com>
2021-08-26 04:53:01 -0700

    Create C program to output text with printf()

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*
+ * main - print text
+ *
+ * return - program should return 0
+ *
+ */
+int main(void)
+{
+	printf("%s", "with proper grammar, but the outcome is a piece of art,\r\n");
+	return (0);
+}
