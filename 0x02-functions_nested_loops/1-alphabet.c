4e00a0194a0f586fb267281d6c98afcd0798c04e
Billyrogers61 <rogerskim61@gmail.com>
2021-09-01 13:57:23 -0700

    -

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,12 +6,12 @@
  */
 void print_alphabet(void)
 {
-	int letter;
+	char letter;
 
-	for (letter = 97; letter < 123; letter++)
+	for (letter = a; letter <= z; letter++)
 	{
 		_putchar(letter);
 	}
 	_putchar("\n");
-}	       
+}
 	     
