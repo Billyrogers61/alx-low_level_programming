7dca10a18a3a9bdf0888bcd1342c29216d6430ac
Billyrogers61 <rogerskim61@gmail.com>
2021-09-01 15:06:09 -0700

    Add Cprogram file

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabets 10 times
+ */
+void print_alphabet_x10(void)
+{
+	char letter;
+	int count;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		for (count = 1; count < 11; count++)
+		{
+			_putchar(letter);
+		}
+	}
+	_putchar('\n');
+}
