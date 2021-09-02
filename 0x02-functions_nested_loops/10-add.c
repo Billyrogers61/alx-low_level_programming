4eed8dea2c9b2ee2b86d5abc5bab0ce6352d8d02
Billyrogers61 <rogerskim61@gmail.com>
2021-09-02 07:30:04 -0700

    Add twon intergers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - function two add two integers and print the result.
+ * first_num: number to be added to the second integer.
+ * second_num: number to be added to the first integer.
+ * Return: Sum of the first integer and second integer.
+ */
+int add(int first_num, int second_num)
+{
+	int sum;
+
+	sum = (first_num + second_num);
+	return (sum);
+}
