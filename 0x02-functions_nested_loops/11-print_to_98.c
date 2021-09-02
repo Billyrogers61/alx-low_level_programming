770448e0d1798b3c27a088809760bc82382ffd5a
Billyrogers61 <rogerskim61@gmail.com>
2021-09-02 10:33:56 -0700

    Output numbers to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - Function to output integer numbers from a given number to 98.
+ * @n: given number (starting number).
+ */
+void print_to_98(int n)
+{
+	while (n < 98)
+	{
+		if (n != 98)
+		{
+			printf("%d, ", n);
+			n++;
+		}
+		else
+		{
+			printf("%d", n);
+			break;
+		}
+	}
+
+	while (n >= 98)
+	{
+		if (n != 98)
+		{
+			printf("%d, ", n);
+			n--;
+		}
+		else
+		{
+			printf("%d", n);
+			break;
+		}
+	}
+}
