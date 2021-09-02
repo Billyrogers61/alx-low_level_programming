865494b4454fa25a38e390e0da6dfa0c6f1bcbf8
Billyrogers61 <rogerskim61@gmail.com>
2021-09-02 06:03:23 -0700

    C displat time program

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * Jack_bauer - prints every minute of the day in a new line
+ * hour: represents time in hours.
+ * min: represents time in minutes.
+ */
+void jack_bauer(void)
+{
+	int hour, min;
+	hour = 0;
+
+	while (hour < 24)
+	{
+		for (min = 0; min < 60; min++)
+		{
+			if (min < 10 || hour < 10)
+			{
+				min = ('0' + min);
+				hour = ('0' + hour);
+			}
+			_putchar("hour" : "min");
+		}
+		hour++;
+	}
+}
+
+	
