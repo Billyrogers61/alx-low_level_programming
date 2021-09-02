38688b032b28b80e972476d96207a68588e8ff7d
Billyrogers61 <rogerskim61@gmail.com>
2021-09-02 07:04:54 -0700

    Display 24 hour time program

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,26 +1,49 @@
 #include "main.h"
 
 /**
- * jack_bauer - prints every minute of the day in a new line
+ * jack_bauer - prints every minute of the day in a new line.
  * hour: represents time in hours.
  * min: represents time in minutes.
  */
 void jack_bauer(void)
 {
-	int hour;
-	int min;	
-	hour = 0;	
-	while (hour < 24)
+	int time1;
+	int time2;
+	int time3;
+	int time4;
+
+	for (time1 = 0; time1 <=2; time1++)
 	{
-		for (min = 0; min < 60; min++)
+		if (time1 == 2 && time2 == 4)
 		{
-			if (min < 10 || hour < 10)
-			{
-				min = ('0' + min);	
-				hour = ('0' + hour);
-			}
-			_putchar(hour + ':' + min);
+			break;
 		}
-		hour++;
+		for (time2 = 0; time2 <= 9; time2++)
+		{
+			if (time1 == 2 && time2 == 4)
+			{
+				break;
+			}
+			for (time3 = 0; time3 <= 5; time3++)
+			{
+				if (time1 == 2 && time2 == 4)
+				{
+					break;
+				}
+				for (time4 = 0; time4 <= 9; time4++)
+				{
+					if (time1 == 2 && time2 == 4)
+					{
+						break;
+					}
+					_putchar(time1 + '0');
+					_putchar(time2 + '0');
+					_putchar(':');
+					_putchar(time3 + '0');
+					_putchar(time4 + '0');
+					_putchar('\n');
+				}
+			}
+		}
 	}
 }
