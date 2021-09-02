dcfe84242ee000efe1907282a524618423f8e526
Billyrogers61 <rogerskim61@gmail.com>
2021-09-02 06:31:23 -0700

    More changes

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,17 +7,18 @@
  */
 void jack_bauer(void)
 {
-	int hour, min;	
+	int hour;
+	int min;
+	
 	hour = 0;
-
+	
 	while (hour < 24)
 	{
 		for (min = 0; min < 60; min++)
 		{
 			if (min < 10 || hour < 10)
 			{
-				min = ('0' + min);
-				
+				min = ('0' + min);	
 				hour = ('0' + hour);
 			}
 			_putchar(hour + ':' + min);
