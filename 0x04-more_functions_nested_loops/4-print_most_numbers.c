ef1f49519ca62a4deae2a090ba94c3e0c4337091
Billyrogers61 <rogerskim61@gmail.com>
2021-09-03 11:28:29 -0700

    Output most-numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * more_numbers - function to print numbers 0 to 9 except 2 and 4.
+ * print_most_numbers - function to print numbers 0 to 9 except 2 and 4.
  * Return: Nothing to return.
  */
 void print_most_numbers(void)
