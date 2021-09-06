096d250e2a1911a91e53239d0ba064cfc02b2657
Billyrogers61 <rogerskim61@gmail.com>
2021-09-06 11:47:37 -0700

    Print string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - function to print out a string.
+ * @str: pointer variable.
+ * Return: Nothing
+ */
+void _puts(char *str)
+{
+	int x;
+
+	for (x = 0; str[x] != '\0'; x++)
+	{
+		_putchar(*str);
+	}
+	_putchar('\n');
+}
