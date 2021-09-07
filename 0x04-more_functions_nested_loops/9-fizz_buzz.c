7fb2f2bbb7facaf70043e5f1e20e2eb60b875b63
Billyrogers61 <rogerskim61@gmail.com>
2021-09-07 12:29:12 -0700

    Print numbers

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - function to print numbers from 1 to 100 but with conditions.
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int num;
+
+	num = 1;
+	while (num > 0 && num <= 100)
+	{
+		if ((num % 3 == 0) && (num % 5 == 0))
+		{
+			printf("%s ", "FizzBuzz");
+		}
+		else if (num % 3 == 0)
+		{
+			printf("%s ", "Fizz");
+		}
+		else if (num % 5 == 0)
+		{
+			printf("%s ", "Buzz");
+		}
+		else
+		{
+			printf("%d ", num);
+		}
+		num++;
+	}
+	printf("\n");
+	return (0);
+}
