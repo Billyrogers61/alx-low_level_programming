7accfd0c56c65b583d496c8e35513bd88e4ed7b8
Billyrogers61 <rogerskim61@gmail.com>
2021-09-07 07:39:07 -0700

    Copr a string

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,9 +10,10 @@ char *_strcpy(char *dest, char *src)
 {
 	int count;
 
-	for (count = 0; src[count]; count++)
+	for (count = 0; src[count] != '\0'; count++)
 	{
 		dest[count] = src[count];
 	}
+	dest[count] = '\0';
 	return (dest);
 }
