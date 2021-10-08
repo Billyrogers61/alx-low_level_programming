b7547fcd2153a1cc826ed4b989d4e0055fa7a777
Billyrogers61 <rogerskim61@gmail.com>
2021-10-07 17:23:30 -0700

    Linked lists

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+typedef unsigned int size_t;
+/**
+ * list_len - Counts the length of a linked list
+ * @h: pointer to the struct
+ * Return: the number of nodes
+ */
+size_t list_len(const list_t *h)
+{
+	unsigned int i = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		i++;
+	}
+
+	return (i);
+}
